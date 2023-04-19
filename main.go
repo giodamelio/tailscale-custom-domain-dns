6b38b7dce418d47254b9321025b4d3cea9070828
Giovanni d'Amelio <gio@damelio.net>
2023-04-19 10:24:14 -0700

    Super simple and bad dns server

--- main.go
+++ main.go
@@ -1,9 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/miekg/dns"
 )
 
+func parseQuery(m *dns.Msg) {
+	for _, q := range m.Question {
+		switch q.Qtype {
+		case dns.TypeA:
+			log.Printf("Query for %s\n", q.Name)
+			rr, err := dns.NewRR("test A 1.1.1.1")
+			if err == nil {
+				m.Answer = append(m.Answer, rr)
+			}
+		}
+	}
+}
+
+func handleDnsRequest(w dns.ResponseWriter, r *dns.Msg) {
+	log.Println("Handling Request")
+
+	m := new(dns.Msg)
+	m.SetReply(r)
+
+	switch r.Opcode {
+	case dns.OpcodeQuery:
+		parseQuery(m)
+	}
+
+	w.WriteMsg(m)
+}
+
 func main() {
-	fmt.Println("Hello flake")
+	dns.HandleFunc("home.gio.ninja.", handleDnsRequest)
+
+	port := 5353
+	server := &dns.Server{Addr: ":" + strconv.Itoa(port), Net: "udp"}
+	log.Printf("Starting DNS server on port %d\n", port)
+	err := server.ListenAndServe()
+	defer server.Shutdown()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
