22ecaf8ba0ca924eba016606791a979504a49355
Giovanni d'Amelio <gio@damelio.net>
2023-04-23 17:03:02 -0700

    Add help instruction

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
@@ -8,9 +10,35 @@ import (
 	"github.com/giodamelio/tailscale-custom-domain-dns/server"
 )
 
+func printHelp() {
+	log.Info().Msg("Small DNS server that serves records of your Tailnet as a subdomain of any domain")
+	log.Info().Msg("Instructions:")
+	log.Info().Msg("  Generate config with TODO")
+	log.Info().Msg("  Fill in config")
+	log.Info().Msgf("  place %s somewhere in an XDG config directory", configName)
+	log.Info().Msg("  Keep this server running")
+}
+
+func argsContain(flags []string) bool {
+	for _, arg := range os.Args {
+		for _, flag := range flags {
+			if arg == flag {
+				return true
+			}
+		}
+	}
+	return false
+}
+
 func main() {
 	// Setup logging
 	log.Logger = log.Output(createFormatter())
+
+	// Print help if necessary
+	if argsContain([]string{"-h", "-help", "--help", "help"}) {
+		printHelp()
+		os.Exit(0)
+	}
 
 	// Setup the config
 	loadConfig()
